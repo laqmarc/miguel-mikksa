986d7d1747079f9136060de186fe02cbb233c2ec
laqmarc <laqmarc@gmail.com>
2023-06-09 18:26:35 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    buildOptions: {
-        // Set base to the correct value for your repository
-        site: 'https://laqmarc.github.io/',
-        base: '/miguel-mikksa/',
-      }
     
-
+    site: 'https://laqmarc.github.io/',
+    base: '/miguel-mikksa/',
 });
